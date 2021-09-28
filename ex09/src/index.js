2bb90bc910dbe2d31309d1867cb840e6e9efcd45
vladimir <vladimir.obradovic.sa@hotmail.com>
2021-09-28 10:40:50 +0200

    last push

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+   
+// only change code below this line
+const product = (...args) => {
+    // only change code above this line
+    return args.reduce((a, b) => a * b, 1);
+}
+
+console.log(product(4, 3, 10, 2)); // change this line
+
+module.exports = product;
